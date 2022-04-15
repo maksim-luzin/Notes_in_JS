6c4f7fc74d2032281e399620ba599af829c5d961
maksim-luzin <333maximax333@gmail.com>
2022-04-15 19:43:51 +0300

    Edit save note

--- src/javascript/service/saveNote.js
+++ src/javascript/service/saveNote.js
@@ -6,6 +6,8 @@ const saveNote = () => {
   const id = getRoute()[1];
   const updateContent = getDataForm();
 
+  if (!updateContent.content || !updateContent.name) return;
+
   updateNoteById(id, updateContent);
   setRoute(Route.Active);
 };
